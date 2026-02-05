3d0312c5e80b8114ce06260806bb47103e6e1579
ThreeDeeJay <71472458+ThreeDeeJay@users.noreply.github.com>
2026-02-05 01:36:01 -0500

    Downgrade to Vulkan 1.1 for compatibility 425.31

--- src/standalone/sdl-mono.cpp
+++ src/standalone/sdl-mono.cpp
@@ -175,7 +175,7 @@ private:
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_3;
+        appInfo.apiVersion = VK_API_VERSION_1_1;
 
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
